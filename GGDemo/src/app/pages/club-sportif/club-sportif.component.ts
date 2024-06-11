0a8f49e41e619809b792b46528c0910a4fe48c92
Gaban0I <glt037100@gmail.com>
2024-06-11 20:17:47 +0200

    adaptation responsive pages

--- GGDemo/src/app/pages/club-sportif/club-sportif.component.ts
+++ GGDemo/src/app/pages/club-sportif/club-sportif.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { TitreBandeauComponent } from '../titre-bandeau/titre-bandeau.component';
-import { ContactUsBandeComponent } from '../contact-us-bande/contact-us-bande.component';
+import { TitreBandeauComponent } from '../../titre-bandeau/titre-bandeau.component';
+import { ContactUsBandeComponent } from '../../contact-us-bande/contact-us-bande.component';
 
 @Component({
   selector: 'app-club-sportif',
